f29e2c16f48fa7e309f2e7beb4ec94b8898fc4a6
Joshua Davian <joshuadavian@gmail.com>
2023-11-14 11:09:31 +0700

    search form component

--- resources/js/Components/app/SearchForm.vue
+++ resources/js/Components/app/SearchForm.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
-        Search Form
-    </div>
+    <form class="w-[600px] h-[80px] flex items-center">
+        <TextInput type="text" class="block w-full mr-2" v-model="form.search" autocomplete
+            placeholder="Search for files and folder..." />
+    </form>
 </template>
 
-<script setup></script>
+<script setup>
+import TextInput from '@/Components/TextInput.vue';
+import { useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+    search: ''
+});
+</script>
