f7760c23c9ad49a01232ea1b8ed6cacad03658c7
Joshua Davian <joshuadavian@gmail.com>
2023-11-30 09:54:59 +0700

    my files page initial layout

--- resources/js/Components/app/Navigation.vue
+++ resources/js/Components/app/Navigation.vue
@@ -1,17 +1,15 @@
 <template>
     <nav class="min-w-[200px]">
-        <div class="h-[80px] px-3 flex items-center gap-3">
-            <Link :href="route('dashboard')">
-            <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
-            </Link>
-            LaraDrive
-        </div>
+        <Link :href="route('file.my-files')" class="h-[80px] px-3 flex items-center gap-3">
+        <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
+        LaraDrive
+        </Link>
 
         <div class="px-3">
             <CreateNewDropdown />
 
             <div class="py-3">
-                <NavLink href="/">My Files</NavLink>
+                <NavLink :href="route('file.my-files')" :active="$page.url === '/my-files'">My Files</NavLink>
                 <NavLink href="/">Shared With Me</NavLink>
                 <NavLink href="/">Shared By Me</NavLink>
                 <NavLink href="/">Trash</NavLink>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -29,10 +30,16 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::controller(FileController::class)->name('file.')->group(function () {
+        Route::get('/my-files', 'myFiles')->name('my-files');
+    });
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
